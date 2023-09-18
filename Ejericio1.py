33c3964afb064553a08997ff975ed4cac9823dc6
Nacho <isanccal@myuax.com>
2023-09-18 10:57:20 +0200

    f

--- Ejericio1.py
+++ Ejericio1.py
@@ -5,4 +5,21 @@ tablero = [
     [None, 0, None]
 ]
 
-movimientos = 
+n = 2
+
+def contar_movimientos(tablero, n):
+
+movimientos = {
+    0: [6, 4], 
+    1: [6, 8], 
+    2: [7, 9], 
+    3: [4, 8], 
+    4: [3, 9, 0],
+    5: [],
+    6: [1, 7, 0],
+    7: [2, 6],
+    8: [1, 3], 
+    9: [2, 4], 
+
+}
+
